8766b59fb3fa300e382ddfcf6e1d9f6ab4303b73
ahlam alshaba <ama1977ln@outlook.com>
2021-03-11 11:26:17 +0100

    done

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -4,9 +4,9 @@ function initMap() {
     center: {lat: 45.2801, lng: 9.1124},
     zoom: 8
   });
-  
- new google.maps.Marker ({
-  position: {lat: 45.2801, lng: 9.1124},
-  map: map
-});
+  new google.maps.Marker({
+    position: {lat: 45.2801, lng: 9.1124},
+    map: map
+  });
 }
+

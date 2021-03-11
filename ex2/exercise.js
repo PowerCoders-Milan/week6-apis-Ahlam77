82da281c54124e8db1cf36061b4cd8856a724464
ahlam alshaba <ama1977ln@outlook.com>
2021-03-11 13:44:26 +0100

    done

---
+++ ex2/exercise.js
@@ -0,0 +1,51 @@
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Barlin&appid=65daeff6b19a4d6bb23b5f9ead176075')
+    .then(response => response.json())
+    .then( data => {
+        var body = document.querySelector("body")
+        var p = document.querySelector("div")
+        var p1 = document.createElement("p")
+        var p2 = document.createElement("p")
+        var p3 = document.createElement("p")
+        body.appendChild(p)
+        p.appendChild(p1)
+        p.appendChild(p2)
+        p.appendChild(p3)
+        p1.innerHTML = data.name
+        p2.innerHTML = data.main.temp
+        p3.innerHTML = data.weather[0].description
+    });
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=65daeff6b19a4d6bb23b5f9ead176075')
+    .then(response => response.json())
+    .then( data => {
+        var body = document.querySelector("body")
+        var p = document.querySelector("div")
+        var p1 = document.createElement("p")
+        var p2 = document.createElement("p")
+        var p3 = document.createElement("p")
+        body.appendChild(p)
+        p.appendChild(p1)
+        p.appendChild(p2)
+        p.appendChild(p3)
+        p1.innerHTML = data.name
+        p2.innerHTML = data.main.temp
+        p3.innerHTML = data.weather[0].description
+        });
+        
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Libya&appid=65daeff6b19a4d6bb23b5f9ead176075')
+    .then(response => response.json())
+    .then( data => {
+        var body = document.querySelector("body")
+        var p = document.querySelector ("div")
+        var p1 = document.createElement("p")
+        var p2 = document.createElement("p")
+        var p3 = document.createElement("p")
+        body.appendChild(p)
+        p.appendChild(p1)
+        p.appendChild(p2)
+        p.appendChild(p3)
+        p1.innerHTML = data.name
+        p2.innerHTML = data.main.temp
+        p3.innerHTML = data.weather[0].description
+        });
